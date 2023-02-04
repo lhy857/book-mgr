8b06c70e417036df2ad44f25da4dbaec5d4684c7
showmaker <1359777852@qq.com>
2023-02-04 18:47:15 +0800

    完成前端登入注册相关的界面

--- book-mgr-fe/src/views/Auth/index.js
+++ book-mgr-fe/src/views/Auth/index.js
@@ -1,8 +1,13 @@
 // 提供一些配置项给vue
 import { defineComponent } from 'vue';
-
+import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons-vue';
 
 export default defineComponent({
+   components: {
+      UserOutlined,
+      LockOutlined,
+      MailOutlined,
+   },
    setup() {
 
    },
